e7758498b45c55fa3de7c9414c0afb64b89f1793
Niall O'Brien <me@niallobrien.me>
2016-03-14 13:40:12 +0000

    > Adds pjax functions

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,20 @@
 import pjax from 'simple-pjax'
 
+// Timeout before calling the loading indicator function. Set to 0 to disable.
+pjax.indicateLoadAfter = 100
+
+// Called when loading takes a while. Use it to display a custom loading indicator.
+pjax.onIndicateLoadStart = function() {
+  document.documentElement.style.opacity = 0.5
+}
+
+// Called when loading ends. Use it to hide a custom loading indicator.
+pjax.onIndicateLoadEnd = function() {
+  document.documentElement.style.opacity = null
+}
+
 $(document).ready(function() {
 
   // Place JavaScript code here...
-  console.log('testing')
-});
+
+})
